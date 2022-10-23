4f6b430847f0b61af576908db827ea1c8e2b8431
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-23 12:19:23 +0300

    init: add Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import './Header.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
+import logo from '../../images/logo.svg'
+
+function Header () {
+
+    const paths = [
+        '/movies',
+        '/saved-movies',
+        '/profile',
+    ];
+
+    return (
+        <Switch>
+            <Route path={paths}>
+                <header className='header'>
+                    <Link to="/">
+                        <img
+                            className="logo"
+                            src={logo}
+                            alt="Логотип"
+                        />
+                    </Link>
+                    <Navigation />
+                </header>
+            </Route>
+            <Route path={paths}>
+                <header className='header'>
+                    <Link to="/">
+                        <img
+                            className="logo"
+                            src={logo}
+                            alt="Логотип"
+                        />
+                    </Link>
+                    <Navigation />
+                </header>
+            </Route>
+        </Switch>
+    );
+
+};
+
+export default Header;
