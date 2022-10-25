29b8d47fc8aad1da5a97cfecc61a3664e672efbc
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-25 15:59:52 +0300

    feat: add AboutProject

--- src/components/Caption/Caption.jsx
+++ src/components/Caption/Caption.jsx
@@ -1,4 +1,4 @@
-import './Caption/Caption';
+import './Caption.css';
 
 function Caption({ text }) {
     
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import './Main.css';
 import Promo from "../Promo/Promo";
 // import NavTab from "../NavTab/NavTab";
-// import AboutProject from "../AboutProject/AboutProject";
+import AboutProject from "../AboutProject/AboutProject";
 // import Techs from "../Techs/Techs";
 // import AboutMe from "../AboutMe/AboutMe";
 // import Portfolio from '../Portfolio/Portfolio';
@@ -11,8 +11,9 @@ function Main() {
     return (
         <main className='main'>
             <Promo />
-            {/* <NavTab />
             <AboutProject />
+            {/* <NavTab />
+            
             <Techs />
             <AboutMe />
             <Portfolio /> */}
