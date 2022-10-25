1b27881c8aa9839af5649ee79107d5082fafcfe7
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-25 14:05:54 +0300

    feat: add Caption

---
+++ src/components/Caption/Caption.jsx
@@ -0,0 +1,13 @@
+import './Caption/Caption';
+
+function Caption({ text }) {
+    
+    return (
+        <h2 className='caption'>
+            {text}
+        </h2>
+    );
+    
+};
+
+export default Caption;
