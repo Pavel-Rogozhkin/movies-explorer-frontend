58dfcdab8f5acdd41519487bbacfa5ea6b94a42d
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-29 18:01:56 +0300

    feat: PortfolioItem

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
-// import Portfolio from '../Portfolio/Portfolio';
+import Portfolio from '../Portfolio/Portfolio';
 
 function Main() {
 
@@ -13,8 +13,7 @@ function Main() {
             <AboutProject />
             <Techs />
             <AboutMe />
-            {/* 
-            <Portfolio /> */}
+            <Portfolio />
         </main>
     );
 
