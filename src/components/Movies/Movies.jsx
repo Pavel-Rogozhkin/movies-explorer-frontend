0c3e2f753641f916e25a1b142d4f12a110293b61
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-29 18:21:27 +0300

    feat: add init Movies

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,14 @@
+import './Movies.css';
+import SearchForm from '../SearchForm/SearchForm';
+
+function Movies() {
+
+    return (
+        <>
+            <SearchForm />
+        </>
+    );
+
+};
+
+export default Movies;
