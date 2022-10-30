39a3ad9b6f2310869903f0b3c3fa2055d954e6dd
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-30 21:14:44 +0300

    fix: fix MoviesList

--- src/utils/moviesArray.js
+++ src/utils/moviesArray.js
@@ -1,10 +1,89 @@
 const moviesArray = [
+
     {
         _id: 1,
         title: 'В погоне за Бенкси',
         duration: '27 минут',
         img: require('../images/movies/benksi.png'),
     },
+
+    {
+        _id: 2,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/2.png'),
+    },
+    
+    {
+        _id: 3,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/3.png'),
+    },
+    
+    {
+        _id: 4,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/4.png'),
+    },
+    
+    {
+        _id: 5,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/5.png'),
+    },
+    
+    {
+        _id: 6,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/6.png'),
+    },
+    
+    {
+        _id: 7,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/7.png'),
+    },
+    
+    {
+        _id: 8,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/8.png'),
+    },
+    
+    {
+        _id: 9,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/9.png'),
+    },
+    
+    {
+        _id: 10,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/10.png'),
+    },
+    
+    {
+        _id: 11,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/11.png'),
+    },
+    
+    {
+        _id: 12,
+        title: 'В погоне за Бенкси',
+        duration: '27 минут',
+        img: require('../images/movies/12.png'),
+    },
+    
 ];
 
 export default moviesArray;
