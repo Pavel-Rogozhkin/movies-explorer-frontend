0f79db141b9b7eecf7cc3cbdce5f21bc3be74c5d
Павел Рогожкин <rogozhkinpaul@yandex.ru>
2022-10-31 21:44:39 +0300

    feat: init Profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
-// import Profile from '../Profile/Profile';
+import Profile from '../Profile/Profile';
 // import Login from '../Login/Login';
 // import Register from '../Register/Register';
 import Footer from '../Footer/Footer';
@@ -26,11 +26,11 @@ function App() {
                     <SavedMovies />
                 </Route>
                 
-                {/* <Route path='/profile'>
+                <Route path='/profile'>
                     <Profile />
                 </Route>
                 
-                <Route path='/signin'>
+                {/* <Route path='/signin'>
                     <Login />
                 </Route>
                 
